a62a098e7fa480e5ac1bdb26bc36546cd4bbd582
Gabriel Brasileiro <gah.lucas@gmail.com>
2020-06-07 22:31:05 -0300

    Implementation of person tests

---
+++ sample/src/test/java/com/mvvmredux/sample/person/PersonViewModelTest.kt
@@ -0,0 +1,51 @@
+package com.mvvmredux.sample.person
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.mvvmredux.core.reducer.Reducer
+import com.mvvmredux.sample.modules.person.PersonData
+import com.mvvmredux.sample.modules.person.PersonStateEvent
+import com.mvvmredux.sample.modules.person.PersonViewModel
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.reset
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class PersonViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutor = InstantTaskExecutorRule()
+
+    private val reducer = mock<Reducer<PersonData, PersonStateEvent>>()
+
+    private lateinit var personViewModel: PersonViewModel
+
+    @Before
+    fun setup() {
+        personViewModel = PersonViewModel(reducer)
+    }
+
+    @Before
+    fun tearDown() {
+        reset(reducer)
+    }
+
+    @Test
+    fun `saveAge Should update age`() {
+        val expected = "10"
+
+        personViewModel.saveAge(expected)
+
+        verify(reducer).updateTo(PersonStateEvent.UpdateAge(expected))
+    }
+
+    @Test
+    fun `saveName Should update name`() {
+        val expected = "Gabriel Brasileiro"
+
+        personViewModel.saveName(expected)
+
+        verify(reducer).updateTo(PersonStateEvent.UpdateName(expected))
+    }
+}
